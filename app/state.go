873e78f7ff039f2e62858684b92cc612efa9b0d5
rigelrozanski <rigel.rozanski@gmail.com>
2017-01-11 13:59:14 -0500

    persistent variable passed

--- app/state.go
+++ app/state.go
@@ -5,16 +5,18 @@ import merkle "github.com/tendermint/go-merkle"
 // State represents the app states, separating the commited state (for queries)
 // from the working state (for CheckTx and AppendTx)
 type State struct {
-	committed merkle.Tree
-	deliverTx merkle.Tree
-	checkTx   merkle.Tree
+	committed  merkle.Tree
+	deliverTx  merkle.Tree
+	checkTx    merkle.Tree
+	persistent bool
 }
 
 func NewState(tree merkle.Tree, persistent bool) State {
 	return State{
-		committed: tree,
-		deliverTx: tree.Copy(),
-		checkTx:   tree.Copy(),
+		committed:  tree,
+		deliverTx:  tree.Copy(),
+		checkTx:    tree.Copy(),
+		persistent: persistent,
 	}
 }
 
@@ -34,8 +36,13 @@ func (s State) Check() merkle.Tree {
 // starts new Append/Check state, and
 // returns the hash for the commit
 func (s *State) Commit() []byte {
-	// TODO: use save, broken for now, cuz it requires a backing db
-	hash := s.deliverTx.Hash()
+	var hash []byte
+	if s.persistent {
+		hash = s.deliverTx.Save()
+	} else {
+		hash = s.deliverTx.Hash()
+	}
+
 	s.committed = s.deliverTx
 	s.deliverTx = s.committed.Copy()
 	s.checkTx = s.committed.Copy()
