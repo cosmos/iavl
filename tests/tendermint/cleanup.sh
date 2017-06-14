c4bc4e905f9e625101dbfacedb4009975ca97bae
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-06-14 18:59:33 +0200

    kill-tendermerk works on osx, added cleanup script

---
+++ tests/tendermint/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Cleaning up old test results"
+
+rm -rf orphan-test-db
+rm merkleeyes.log
+rm tendermint.log
--- tests/tendermint/kill-tendermerk.sh
+++ tests/tendermint/kill-tendermerk.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-TPID=`pidof tendermint`
-MPID=`pidof merkleeyes`
-
 echo "Stopping Merkleeyes and Tendermint"
-
-if [ -n "$TPID" ]; then 
-    kill $TPID
-fi
-
-if [ -n "$MPID" ]; then 
-    kill $MPID
-fi
+killall tendermint
+killall merkleeyes
