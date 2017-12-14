2f9a7b43a08e3bd57a1d8ec7c8c44f795f416dce
Alexis Sellier <alexis@cloudhead.io>
2017-12-14 15:57:20 +0100

    Update/improve tests

--- serialize_test.go
+++ serialize_test.go
@@ -9,6 +9,7 @@ import (
 
 // TreeSize is the number of nodes in our test trees
 const TreeSize = 10000
+const TreeVariations = 1
 
 func TestSerialize(t *testing.T) {
 	require := require.New(t)
@@ -18,31 +19,33 @@ func TestSerialize(t *testing.T) {
 		restore   RestoreFunc
 		sameHash  bool
 	}{
-		{InOrderSerialize, RestoreUsingDepth, false},
+		{InOrderSerialize, RestoreUsingDepth, true},
 		{StableSerializeFrey, Restore, true},
 		{StableSerializeBFS, Restore, true},
 	}
 
 	for i, tc := range cases {
-		tree := makeRandomTree(TreeSize)
+		for j := 0; j < TreeVariations; j++ {
+			tree := makeRandomTree(TreeSize)
+			stored := tc.serialize(tree, tree.root)
+			require.NotNil(stored, "%d", i)
+			require.Equal(tree.Size(), len(stored), "%d", i)
+			origHash := tree.Hash()
+			require.NotNil(origHash)
 
-		stored := tc.serialize(tree, tree.root)
-		require.NotNil(stored, "%d", i)
-		require.Equal(tree.Size(), len(stored), "%d", i)
-		origHash := tree.Hash()
-		require.NotNil(origHash)
+			empty := NewTree(nil, TreeSize)
+			require.Equal(0, empty.Size(), "%d", i)
+			tc.restore(empty, stored)
+			require.Equal(tree.Size(), empty.Size(), "%d", i)
 
-		empty := NewTree(nil, TreeSize)
-		require.Equal(0, empty.Size(), "%d", i)
-		tc.restore(empty, stored)
-		require.Equal(tree.Size(), empty.Size(), "%d", i)
+			newHash := empty.Hash()
+			require.NotNil(newHash)
+			if tc.sameHash {
+				require.Equal(origHash, newHash, "%d", i)
+			} else {
+				require.NotEqual(origHash, newHash, "%d", i)
+			}
 
-		newHash := empty.Hash()
-		require.NotNil(newHash)
-		if tc.sameHash {
-			require.Equal(origHash, newHash, "%d", i)
-		} else {
-			require.NotEqual(origHash, newHash, "%d", i)
 		}
 	}
 }
@@ -52,8 +55,8 @@ func makeRandomTree(nodes int) *Tree {
 	tree := NewTree(db.NewMemDB(), nodes)
 
 	for i := 0; i <= nodes; i++ {
-		k := []byte(randstr(16))
-		v := []byte(randstr(32))
+		k := []byte(randstr(5))
+		v := k
 		tree.Set(k, v)
 	}
 	tree.Hash()
