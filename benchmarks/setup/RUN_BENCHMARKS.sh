e5726c0066ccdd299a2ec9262f93c7896cdfcd87
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2018-08-15 02:19:42 +0100

    Release 0.10.0 (#100)
    * dep: Change tendermint dep to be ^v0.22.0 (#91)
    
    * Mutable/Immutable refactor and GetImmutable snapshots (#92)
    
    * Release 0.10.0: Update Changelog and bump version (#99)
    
    See changelog: https://github.com/tendermint/iavl/blob/develop/CHANGELOG.md#0100

--- benchmarks/setup/RUN_BENCHMARKS.sh
+++ benchmarks/setup/RUN_BENCHMARKS.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-. ~/.goenv
+# This runs benchmarks, by default from develop branch of
+# github.com/tendermint/iavl
+# You can customize this by optional command line args
+#
+# INSTALL_USER.sh [branch] [repouser]
+#
+# set repouser as your username to time your fork
 
-MERKLE=$GOPATH/src/github.com/tendermint/merkleeyes/iavl
-cd $MERKLE
-git pull
+BRANCH=${1:-develop}
+REPOUSER=${2:-tendermint}
 
-make get_deps
-make record
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
+export GOPATH=$HOME/go
+
+go get -u github.com/${REPOUSER}/iavl
+cd ~/go/src/github.com/${REPOUSER}/iavl
+git checkout ${BRANCH}
+
+make get_vendor_deps
+make bench > results.txt
+
