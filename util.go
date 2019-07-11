d3af8872bbfd399e10587e5a5edc7645b9dc970a
Marko Baricevic <marbar3778@yahoo.com>
2019-07-11 15:46:03 +0200

    minor wording change

--- util.go
+++ util.go
@@ -145,8 +145,8 @@ func Cyan(args ...interface{}) string {
 	return treatAll(ANSIFgCyan, args...)
 }
 
-// Coloredbytes takes in the byte that you would like to show as a string and byte
-// and display them in different colors, if `TENDERMINT_IAVL_COLORS_ON` is set to on.
+// ColoredBytes takes in the byte that you would like to show as a string and byte
+// and will display them in different colors for readability, if `TENDERMINT_IAVL_COLORS_ON` is set to on.
 func ColoredBytes(data []byte, textColor, bytesColor func(...interface{}) string) string {
 	// If TENDERMINT_IAVL_COLORS_ON is set to on then colors will be used for bytes and strings
 	colors := os.Getenv("TENDERMINT_IAVL_COLORS_ON")
