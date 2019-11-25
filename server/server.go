e6f82899eeda82dda779e6317534957049cfb458
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-11-25 18:47:17 +1100

    Implement proof proto methods

--- server/server.go
+++ server/server.go
@@ -206,17 +206,40 @@ func (s *IAVLServer) VersionExists(_ context.Context, req *pb.VersionExistsReque
 
 // Verify verifies an IAVL range proof returning an error if the proof is invalid.
 func (s *IAVLServer) Verify(ctx context.Context, req *pb.VerifyRequest) (*empty.Empty, error) {
-	panic("not implemented")
+	proof := iavl.ConvertProtoRangeProof(req.Proof)
+	if err := proof.Verify(req.RootHash); err != nil {
+		return nil, err
+	}
+
+	return &empty.Empty{}, nil
 }
 
 // VerifyItem verifies if a given key/value pair in an IAVL range proof returning
 // an error if the proof or key is invalid.
 func (s *IAVLServer) VerifyItem(ctx context.Context, req *pb.VerifyItemRequest) (*empty.Empty, error) {
-	panic("not implemented")
+	proof := iavl.ConvertProtoRangeProof(req.Proof)
+	if err := proof.Verify(req.RootHash); err != nil {
+		return nil, err
+	}
+
+	if err := proof.VerifyItem(req.Key, req.Value); err != nil {
+		return nil, err
+	}
+
+	return &empty.Empty{}, nil
 }
 
 // VerifyAbsence verifies the absence of a given key in an IAVL range proof
 // returning an error if the proof or key is invalid.
 func (s *IAVLServer) VerifyAbsence(ctx context.Context, req *pb.VerifyAbsenceRequest) (*empty.Empty, error) {
-	panic("not implemented")
+	proof := iavl.ConvertProtoRangeProof(req.Proof)
+	if err := proof.Verify(req.RootHash); err != nil {
+		return nil, err
+	}
+
+	if err := proof.VerifyAbsence(req.Key); err != nil {
+		return nil, err
+	}
+
+	return &empty.Empty{}, nil
 }
