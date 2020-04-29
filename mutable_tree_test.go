c31a2cab214b2b3e0672e7a59ccd74c71491c9f0
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-04-29 11:49:21 -0400

    Implement TestFlushVersion

--- mutable_tree_test.go
+++ mutable_tree_test.go
@@ -10,6 +10,83 @@ import (
 	db "github.com/tendermint/tm-db"
 )
 
+func TestFlushVersion(t *testing.T) {
+	memDB := db.NewMemDB()
+	opts := PruningOptions(5, 1)
+
+	tree, err := NewMutableTreeWithOpts(memDB, db.NewMemDB(), 0, opts)
+	require.NoError(t, err)
+	require.NotNil(t, tree)
+
+	// set key/value pairs and commit up to KeepEvery
+	rootHashes := make([][]byte, 0)
+	for i := int64(0); i < opts.KeepEvery; i++ {
+		tree.set([]byte(fmt.Sprintf("key-%d", i)), []byte(fmt.Sprintf("value-%d", i)))
+
+		rh, v, err := tree.SaveVersion()
+		require.NoError(t, err)
+		require.Equal(t, i+1, v)
+
+		rootHashes = append(rootHashes, rh)
+	}
+
+	// verify the latest version
+	require.Equal(t, int64(5), tree.Version())
+
+	// verify we only have the 1st and KeepEvery version flushed to disk
+	for i, rh := range rootHashes {
+		version := int64(i + 1)
+
+		ok, err := tree.ndb.HasSnapshot(rh)
+		require.NoError(t, err)
+
+		if version == 1 || version%opts.KeepEvery == 0 {
+			require.True(t, ok)
+		} else {
+			require.False(t, ok)
+		}
+	}
+
+	// set key/value pairs and commit 2 more times (no flush to disk should occur)
+	for i := opts.KeepEvery; i < opts.KeepEvery+2; i++ {
+		tree.set([]byte(fmt.Sprintf("key-%d", i)), []byte(fmt.Sprintf("value-%d", i)))
+
+		rh, v, err := tree.SaveVersion()
+		require.NoError(t, err)
+		require.Equal(t, i+1, v)
+
+		rootHashes = append(rootHashes, rh)
+	}
+
+	// verify the latest version
+	require.Equal(t, int64(7), tree.Version())
+
+	// verify we do not have the latest version flushed to disk
+	ok, err := tree.ndb.HasSnapshot(rootHashes[len(rootHashes)-1])
+	require.NoError(t, err)
+	require.False(t, ok)
+
+	// verify flushing already flushed version fails
+	require.Error(t, tree.FlushVersion(5))
+
+	// verify we can flush the latest version
+	require.NoError(t, tree.FlushVersion(tree.Version()))
+
+	// verify we do have the latest version flushed to disk
+	ok, err = tree.ndb.HasSnapshot(rootHashes[len(rootHashes)-1])
+	require.NoError(t, err)
+	require.True(t, ok)
+
+	tree2, err := NewMutableTreeWithOpts(memDB, db.NewMemDB(), 0, opts)
+	require.NoError(t, err)
+	require.NotNil(t, tree2)
+
+	// verify we can load the previously manually flushed version on a new tree
+	v, err := tree2.LoadVersion(tree.Version())
+	require.NoError(t, err)
+	require.Equal(t, tree.Version(), v)
+}
+
 func TestDelete(t *testing.T) {
 	memDb := db.NewMemDB()
 	tree, err := NewMutableTree(memDb, 0)
