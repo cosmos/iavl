80c65017700301a3d9169421f14eb2dcb2fabe9f
Erik Grinaker <erik@interchain.berlin>
2020-06-26 10:39:15 +0200

    comment tweak

--- repair.go
+++ repair.go
@@ -9,25 +9,25 @@ import (
 
 // Repair013Orphans repairs incorrect orphan entries written by IAVL 0.13 pruning. To use it, close
 // a database using IAVL 0.13, make a backup copy, and then run this function before opening the
-// database with IAVL 0.14 or later. It returns the number of faulty orphan entries removed.
+// database with IAVL 0.14 or later. It returns the number of faulty orphan entries removed. If the
+// 0.13 database was written with KeepEvery:1 (the default) or the last version _ever_ saved to the
+// tree was a multiple of `KeepEvery` and thus saved to disk, this repair is not necessary.
 //
 // Note that this cannot be used directly on Cosmos SDK databases, since they store multiple IAVL
 // trees in the same underlying database via a prefix scheme.
 //
 // The pruning functionality enabled with Options.KeepEvery > 1 would write orphans entries to disk
 // for versions that should only have been saved in memory, and these orphan entries were clamped
-// to the last version persisted to disk instead of the version that generated them. If the
+// to the last version persisted to disk instead of the version that generated them (so a delete at
+// version 749 might generate an orphan entry ending at version 700 for KeepEvery:100). If the
 // database is reopened at the last persisted version and this version is later deleted, the
 // orphaned nodes can be deleted prematurely or incorrectly, causing data loss and database
 // corruption.
 //
 // This function removes these incorrect orphan entries by deleting all orphan entries that have a
 // to-version equal to or greater than the latest persisted version. Correct orphans will never
-// have this, since they must have been deleted in the next (non-existent) version for that to be
+// have this, since they must have been deleted in a future (non-existent) version for that to be
 // the case.
-//
-// If 0.13 is running with KeepEvery:1 (the default), or if the latest version _ever_ saved to the
-// IAVL tree is persisted to disk (i.e. a multiple of KeepEvery), then this repair is not necessary.
 func Repair013Orphans(db dbm.DB) (uint64, error) {
 	ndb := newNodeDB(db, 0, &Options{Sync: true})
 	version := ndb.getLatestVersion()
