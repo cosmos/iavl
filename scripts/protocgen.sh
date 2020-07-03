2907cc02fe3b974ada4193c9a4a023daf0fa5955
Erik Grinaker <erik@interchain.berlin>
2020-07-03 18:04:24 +0200

    moved Protobuf schemas back to proto/

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,10 +2,13 @@
 
 set -eo pipefail
 
-proto_dirs=$(find ./internal/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   protoc \
-  -I "internal/proto" \
-  --gogofaster_out=$dir \
+  -I "proto" \
+  --gogofaster_out=source_relative:./internal/proto \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
+
+mv ./internal/proto/iavl/* ./internal/proto
+rm -rf ./internal/proto/iavl
