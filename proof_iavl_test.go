89524cdc51bea0f3c3544aec749991f14cffae35
Marko <marbar3778@yahoo.com>
2020-07-14 17:43:44 +0200

    update to latest tm (#294)
    * update to latest tm
    
    * run go mod tidy

--- proof_iavl_test.go
+++ proof_iavl_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	tmmerkle "github.com/tendermint/tendermint/proto/tendermint/crypto/merkle"
+	tmmerkle "github.com/tendermint/tendermint/proto/tendermint/crypto"
 	db "github.com/tendermint/tm-db"
 )
 
