41c182766c06304f851bfc7e5168aef56de516d7
Roman Akhtariev <ackhtariev@gmail.com>
2022-01-14 17:57:37 -0800

    improve mutable tree unit tests related to new functionality

--- mutable_tree.go
+++ mutable_tree.go
@@ -565,7 +565,6 @@ func (tree *MutableTree) SaveVersion() ([]byte, int64, error) {
 	}
 
 	if err := tree.saveFastNodeVersion(); err != nil {
-		tree.ndb.resetBatch()
 		return nil, version, err
 	}
 
@@ -626,9 +625,14 @@ func (tree *MutableTree) saveFastNodeRemovals() error {
 	return nil
 }
 
-// getUnsavedFastNodeChanges returns unsaved FastNodes, used for unit testing
-func (tree *MutableTree) getUnsavedFastNodeChanges() map[string]*FastNode {
+// GetUnsavedFastNodeAdditions returns unsaved FastNodes to add, used for unit testing
+func (tree *MutableTree) GetUnsavedFastNodeAdditions() map[string]*FastNode {
 	return tree.unsavedFastNodeAdditions
+}
+
+// GetUnsavedFastNodeAdditions returns unsaved FastNodes to remove, used for unit testing
+func (tree *MutableTree) GetUnsavedFastNodeRemovals() map[string]interface{} {
+	return tree.unsavedFastNodeRemovals
 }
 
 func (tree *MutableTree) deleteVersion(version int64) error {
