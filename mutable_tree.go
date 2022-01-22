c766f074ff4d23ba5dc8ea949c7d3235e863b1ba
Roman Akhtariev <ackhtariev@gmail.com>
2022-01-22 10:13:05 -0800

    auto set fast version in SaveVersion

--- mutable_tree.go
+++ mutable_tree.go
@@ -748,6 +748,13 @@ func (tree *MutableTree) saveFastNodeVersion() error {
 	if err := tree.saveFastNodeRemovals(); err != nil {
 		return err
 	}
+
+	if !tree.ndb.isFastStorageEnabled() {
+		if err := tree.ndb.setStorageVersionBatch(fastStorageVersionValue); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
