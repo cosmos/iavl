640ad78af7f34486f4793b3e442c8d20c751daf0
luohaha <18810541851@163.com>
2022-06-10 20:53:21 +0800

    add Statistics for cache miss rate

--- options.go
+++ options.go
@@ -1,5 +1,37 @@
 package iavl
 
+import "sync/atomic"
+
+// Statisc about db runtime state
+type Statistics struct {
+	// Each time GetNode and GetFastNode operation hit cache
+	cacheHitCnt uint64
+
+	// Each time GetNode and GetFastNode operation miss cache
+	cacheMissCnt uint64
+}
+
+func (stat *Statistics) IncCacheHitCnt() {
+	atomic.AddUint64(&stat.cacheHitCnt, 1)
+}
+
+func (stat *Statistics) IncCacheMissCnt() {
+	atomic.AddUint64(&stat.cacheMissCnt, 1)
+}
+
+func (stat *Statistics) GetCacheHitCnt() uint64 {
+	return atomic.LoadUint64(&stat.cacheHitCnt)
+}
+
+func (stat *Statistics) GetCacheMissCnt() uint64 {
+	return atomic.LoadUint64(&stat.cacheMissCnt)
+}
+
+func (stat *Statistics) Reset() {
+	atomic.StoreUint64(&stat.cacheHitCnt, 0)
+	atomic.StoreUint64(&stat.cacheMissCnt, 0)
+}
+
 // Options define tree options.
 type Options struct {
 	// Sync synchronously flushes all writes to storage, using e.g. the fsync syscall.
@@ -10,6 +42,9 @@ type Options struct {
 	// this, an error is returned when loading the tree. Only used for the initial SaveVersion()
 	// call.
 	InitialVersion uint64
+
+	// When Stat is not nil, statistical logic needs to be executed
+	Stat *Statistics
 }
 
 // DefaultOptions returns the default options for IAVL.
