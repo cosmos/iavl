a16adce0a71fcfd0f76e811ee455bca02b3a8ffd
faddat <jacobgadikian@gmail.com>
2022-08-22 07:31:43 +0000

    remove bolt and badger

--- benchmarks/bench_test.go
+++ benchmarks/bench_test.go
@@ -260,11 +260,9 @@ func BenchmarkMedium(b *testing.B) {
 	benchmarks := []benchmark{
 		{"memdb", 100000, 100, 16, 40},
 		{"goleveldb", 100000, 100, 16, 40},
-		// {"cleveldb", 100000, 100, 16, 40},
-		// FIXME: idk why boltdb is too slow !?
-		// {"boltdb", 100000, 100, 16, 40},
-		// {"rocksdb", 100000, 100, 16, 40},
-		// {"badgerdb", 100000, 100, 16, 40},
+		{"cleveldb", 100000, 100, 16, 40},
+		{"rocksdb", 100000, 100, 16, 40},
+		{"pebbledb", 100000, 100, 16, 40},
 	}
 	runBenchmarks(b, benchmarks)
 }
@@ -273,10 +271,9 @@ func BenchmarkSmall(b *testing.B) {
 	benchmarks := []benchmark{
 		{"memdb", 1000, 100, 4, 10},
 		{"goleveldb", 1000, 100, 4, 10},
-		// {"cleveldb", 1000, 100, 4, 10},
-		// {"boltdb", 1000, 100, 4, 10},
-		// {"rocksdb", 1000, 100, 4, 10},
-		// {"badgerdb", 1000, 100, 4, 10},
+		{"cleveldb", 1000, 100, 4, 10},
+		{"rocksdb", 1000, 100, 4, 10},
+		{"pebbledb", 1000, 100, 4, 10},
 	}
 	runBenchmarks(b, benchmarks)
 }
@@ -325,7 +322,7 @@ func runBenchmarks(b *testing.B, benchmarks []benchmark) {
 
 		// prepare a dir for the db and cleanup afterwards
 		dirName := fmt.Sprintf("./%s-db", prefix)
-		if (bb.dbType == db.RocksDBBackend) || (bb.dbType == db.CLevelDBBackend) || (bb.dbType == db.BoltDBBackend) {
+		if (bb.dbType == db.RocksDBBackend) || (bb.dbType == db.CLevelDBBackend) {
 			_ = os.Mkdir(dirName, 0755)
 		}
 
