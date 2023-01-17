d9bbe6f8c73d8c840e7540e4c558c5e3d6d67f5a
Yiming Zang <yzang@twitter.com>
2023-01-17 04:54:30 -0800

    Improve IAVL import speed with goroutines

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/cosmos/cosmos-db v0.0.0-20220822060143-23a8145386c0
 	github.com/golang/mock v1.6.0
 	github.com/golangci/golangci-lint v1.50.1
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.1
+	github.com/tendermint/tm-db v0.6.7
 	golang.org/x/crypto v0.5.0
 )
 
@@ -34,6 +36,7 @@ require (
 	github.com/breml/bidichk v0.2.3 // indirect
 	github.com/breml/errchkjson v0.3.0 // indirect
 	github.com/butuzov/ireturn v0.1.1 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/charithe/durationcheck v0.0.9 // indirect
 	github.com/chavacava/garif v0.0.0-20220630083739-93517212f375 // indirect
@@ -42,10 +45,15 @@ require (
 	github.com/cockroachdb/pebble v0.0.0-20220817183557-09c6e030a677 // indirect
 	github.com/cockroachdb/redact v1.0.8 // indirect
 	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
+	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/curioswitch/go-reassign v0.2.0 // indirect
 	github.com/daixiang0/gci v0.8.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/denis-tingaikin/go-header v0.4.3 // indirect
+	github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
+	github.com/dgraph-io/ristretto v0.0.3-0.20200630154024-f66de99634de // indirect
+	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/esimonov/ifshort v1.0.4 // indirect
 	github.com/ettle/strcase v0.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
@@ -129,7 +137,6 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/phayes/checkstyle v0.0.0-20170904204023-bfd46e6a821d // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/polyfloyd/go-errorlint v1.0.5 // indirect
 	github.com/prometheus/client_golang v1.12.2 // indirect
@@ -177,12 +184,14 @@ require (
 	github.com/yagipy/maintidx v1.0.0 // indirect
 	github.com/yeya24/promlinter v0.2.0 // indirect
 	gitlab.com/bosi/decorder v0.2.3 // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.22.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/exp/typeparams v0.0.0-20220827204233-334a2380cb91 // indirect
 	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
--- import.go
+++ import.go
@@ -2,17 +2,24 @@ package iavl
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
+	"sync"
+	"time"
 
-	db "github.com/cosmos/cosmos-db"
+	"github.com/pkg/errors"
+
+	db "github.com/tendermint/tm-db"
 )
 
 // maxBatchSize is the maximum size of the import batch before flushing it to the database
-const maxBatchSize = 10000
+const maxBatchSize = 20000
 
 // ErrNoImport is returned when calling methods on a closed importer
 var ErrNoImport = errors.New("no import in progress")
+var TotalAggregateBytes int64
+var TotalSerializeLatency int64
+var TotalSetLatency int64
+var TotalBatchWriteLatency int64
 
 // Importer imports data into an empty MutableTree. It is created by MutableTree.Import(). Users
 // must call Close() when done.
@@ -22,11 +29,23 @@ var ErrNoImport = errors.New("no import in progress")
 // Importer is not concurrency-safe, it is the caller's responsibility to ensure the tree is not
 // modified while performing an import.
 type Importer struct {
-	tree      *MutableTree
-	version   int64
-	batch     db.Batch
-	batchSize uint32
-	stack     []*Node
+	tree         *MutableTree
+	version      int64
+	batch        db.Batch
+	batchMutex   sync.RWMutex
+	batchSize    uint32
+	stack        []*Node
+	chBatch      chan db.Batch
+	chBatchWg    sync.WaitGroup
+	chNode       chan Node
+	chNodeWg     sync.WaitGroup
+	chNodeData   chan NodeData
+	chNodeDataWg sync.WaitGroup
+}
+
+type NodeData struct {
+	node Node
+	data []byte
 }
 
 // newImporter creates a new Importer for an empty MutableTree.
@@ -38,34 +57,145 @@ func newImporter(tree *MutableTree, version int64) (*Importer, error) {
 		return nil, errors.New("imported version cannot be negative")
 	}
 	if tree.ndb.latestVersion > 0 {
-		return nil, fmt.Errorf("found database at version %d, must be 0", tree.ndb.latestVersion)
+		return nil, errors.Errorf("found database at version %d, must be 0", tree.ndb.latestVersion)
 	}
 	if !tree.IsEmpty() {
 		return nil, errors.New("tree must be empty")
 	}
 
-	return &Importer{
-		tree:    tree,
-		version: version,
-		batch:   tree.ndb.db.NewBatch(),
-		stack:   make([]*Node, 0, 8),
-	}, nil
+	var importer = &Importer{
+		tree:         tree,
+		version:      version,
+		batch:        tree.ndb.db.NewBatch(),
+		batchMutex:   sync.RWMutex{},
+		stack:        make([]*Node, 0, 8),
+		chBatch:      make(chan db.Batch, 1),
+		chBatchWg:    sync.WaitGroup{},
+		chNode:       make(chan Node, maxBatchSize),
+		chNodeWg:     sync.WaitGroup{},
+		chNodeData:   make(chan NodeData, maxBatchSize),
+		chNodeDataWg: sync.WaitGroup{},
+	}
+
+	importer.chBatchWg.Add(1)
+	go periodicBatchCommit(importer)
+
+	importer.chNodeWg.Add(1)
+	go serializeAsync(importer)
+
+	importer.chNodeDataWg.Add(1)
+	go writeNodeData(importer)
+
+	return importer, nil
+}
+
+func periodicBatchCommit(i *Importer) {
+	for i.batch != nil {
+		nextBatch, chanOpen := <-i.chBatch
+		if !chanOpen {
+			break
+		}
+		batchWriteStart := time.Now().UnixMicro()
+		err := nextBatch.Write()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Closing batch after batch write done")
+		nextBatch.Close()
+		batchWriteEnd := time.Now().UnixMicro()
+		batchCommitLatency := batchWriteEnd - batchWriteStart
+		TotalBatchWriteLatency += batchCommitLatency
+		fmt.Printf("[IAVL IMPORTER] Batch commit latency: %d\n", batchCommitLatency/1000)
+
+	}
+	i.chBatchWg.Done()
+	fmt.Printf("[IAVL IMPORTER] Shutting down the batch commit thread\n")
+}
+
+func serializeAsync(i *Importer) {
+	for i.batch != nil {
+		start := time.Now().UnixMicro()
+		currNode, chanOpen := <-i.chNode
+
+		if !chanOpen {
+			break
+		}
+
+		err := currNode.validate()
+		if err != nil {
+			panic(err)
+		}
+
+		buf := bufPool.Get().(*bytes.Buffer)
+		buf.Reset()
+		if err = currNode.writeBytes(buf); err != nil {
+			panic(err)
+		}
+		bytesCopy := make([]byte, buf.Len())
+		copy(bytesCopy, buf.Bytes())
+
+		bufPool.Put(buf)
+
+		TotalAggregateBytes += int64(len(bytesCopy))
+		TotalSerializeLatency += time.Now().UnixMicro() - start
+		i.chNodeData <- NodeData{
+			node: currNode,
+			data: bytesCopy,
+		}
+
+	}
+	i.chNodeWg.Done()
+	fmt.Printf("[IAVL IMPORTER] Shutting down the serializeAsync thread\n")
+}
+
+func writeNodeData(i *Importer) {
+	for i.batch != nil {
+		nodeData, chanOpen := <-i.chNodeData
+		if !chanOpen {
+			break
+		}
+		start := time.Now().UnixMicro()
+		i.batchMutex.RLock()
+		if i.batch != nil {
+			err := i.batch.Set(i.tree.ndb.nodeKey(nodeData.node.hash), nodeData.data)
+			if err != nil {
+				panic(err)
+			}
+		}
+		TotalSetLatency += time.Now().UnixMicro() - start
+		i.batchMutex.RUnlock()
+		i.batchSize++
+		if i.batchSize >= maxBatchSize && len(i.chBatch) < 1 {
+			i.chBatch <- i.batch
+			i.batch = i.tree.ndb.db.NewBatch()
+			i.batchSize = 0
+		}
+	}
+	i.chNodeDataWg.Done()
+	fmt.Printf("[IAVL IMPORTER] Shutting down the writeNodeData thread\n")
 }
 
 // Close frees all resources. It is safe to call multiple times. Uncommitted nodes may already have
 // been flushed to the database, but will not be visible.
 func (i *Importer) Close() {
+	i.batchMutex.Lock()
+	defer i.batchMutex.Unlock()
+	fmt.Println("Acquired lock in close")
+
 	if i.batch != nil {
+		fmt.Println("Closing batch now!!!")
 		i.batch.Close()
 	}
 	i.batch = nil
 	i.tree = nil
+
 }
 
 // Add adds an ExportNode to the import. ExportNodes must be added in the order returned by
 // Exporter, i.e. depth-first post-order (LRN). Nodes are periodically flushed to the database,
 // but the imported version is not visible until Commit() is called.
 func (i *Importer) Add(exportNode *ExportNode) error {
+
 	if i.tree == nil {
 		return ErrNoImport
 	}
@@ -73,7 +203,7 @@ func (i *Importer) Add(exportNode *ExportNode) error {
 		return errors.New("node cannot be nil")
 	}
 	if exportNode.Version > i.version {
-		return fmt.Errorf("node version %v can't be greater than import version %v",
+		return errors.Errorf("node version %v can't be greater than import version %v",
 			exportNode.Version, i.version)
 	}
 
@@ -112,42 +242,9 @@ func (i *Importer) Add(exportNode *ExportNode) error {
 	if node.rightNode != nil {
 		node.size += node.rightNode.size
 	}
+	node._hash()
 
-	_, err := node._hash()
-	if err != nil {
-		return err
-	}
-
-	err = node.validate()
-	if err != nil {
-		return err
-	}
-
-	buf := bufPool.Get().(*bytes.Buffer)
-	buf.Reset()
-	defer bufPool.Put(buf)
-
-	if err = node.writeBytes(buf); err != nil {
-		return err
-	}
-
-	bytesCopy := make([]byte, buf.Len())
-	copy(bytesCopy, buf.Bytes())
-
-	if err = i.batch.Set(i.tree.ndb.nodeKey(node.hash), bytesCopy); err != nil {
-		return err
-	}
-
-	i.batchSize++
-	if i.batchSize >= maxBatchSize {
-		err = i.batch.Write()
-		if err != nil {
-			return err
-		}
-		i.batch.Close()
-		i.batch = i.tree.ndb.db.NewBatch()
-		i.batchSize = 0
-	}
+	i.chNode <- *node
 
 	// Update the stack now that we know there were no errors
 	switch {
@@ -156,6 +253,7 @@ func (i *Importer) Add(exportNode *ExportNode) error {
 	case node.leftHash != nil || node.rightHash != nil:
 		i.stack = i.stack[:stackSize-1]
 	}
+
 	// Only hash\height\size of the node will be used after it be pushed into the stack.
 	i.stack = append(i.stack, &Node{hash: node.hash, subtreeHeight: node.subtreeHeight, size: node.size})
 
@@ -166,34 +264,56 @@ func (i *Importer) Add(exportNode *ExportNode) error {
 // version visible, and updating the tree metadata. It can only be called once, and calls Close()
 // internally.
 func (i *Importer) Commit() error {
+	fmt.Println("[IAVL] Waiting to start commit")
+	for len(i.chNodeData) > 0 || len(i.chNode) > 0 || len(i.chBatch) > 0 {
+		time.Sleep(10 * time.Millisecond)
+	}
+	close(i.chNode)
+	i.chNodeWg.Wait()
+	close(i.chNodeData)
+	i.chNodeDataWg.Wait()
+	close(i.chBatch)
+	i.chBatchWg.Wait()
+
+	fmt.Println("[IAVL] Starting to commit")
+
 	if i.tree == nil {
 		return ErrNoImport
 	}
 
 	switch len(i.stack) {
 	case 0:
+		fmt.Println("[IAVL] Writing something for case 0")
 		if err := i.batch.Set(i.tree.ndb.rootKey(i.version), []byte{}); err != nil {
-			return err
+			panic(err)
 		}
 	case 1:
+		fmt.Println("[IAVL] Writing something for case 1")
 		if err := i.batch.Set(i.tree.ndb.rootKey(i.version), i.stack[0].hash); err != nil {
-			return err
+			panic(err)
 		}
 	default:
-		return fmt.Errorf("invalid node structure, found stack size %v when committing",
+		return errors.Errorf("invalid node structure, found stack size %v when committing",
 			len(i.stack))
 	}
 
+	fmt.Println("[IAVL] Committing batch with write sync")
 	err := i.batch.WriteSync()
 	if err != nil {
+		fmt.Printf("[IAVL] Committing batch hitting some err: %s", err.Error())
 		return err
 	}
+	fmt.Println("[IAVL] Resetting latest version ")
 	i.tree.ndb.resetLatestVersion(i.version)
-
+	fmt.Println("[IAVL] Loading version ")
 	_, err = i.tree.LoadVersion(i.version)
+	fmt.Println("[IAVL] Loaded version ")
 	if err != nil {
 		return err
 	}
+
+	fmt.Printf("[IAVL] Closing batch after commit(), total size is %d \n", TotalAggregateBytes)
+	fmt.Printf("[IAVL] TotalSerialize latency is: %d, TotalSetDataLatency is: %d, TotalBatchWriteLatency is:%d \n", TotalSerializeLatency, TotalSetLatency, TotalBatchWriteLatency)
 
 	i.Close()
 	return nil
