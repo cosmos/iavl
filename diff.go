7541e3adda1aa30a74bc5b309ef9bd65a5db2fd5
Marko Baricevic <marbar3778@yahoo.com>
2023-04-20 12:32:44 +0200

    fix conflicts

--- diff.go
+++ diff.go
@@ -21,7 +21,7 @@ type KVPairReceiver func(pair *KVPair) error
 //
 // The algorithm don't run in constant memory strictly, but it tried the best the only
 // keep minimal intermediate states in memory.
-func (ndb *nodeDB) extractStateChanges(prevVersion int64, prevRoot *NodeKey, root *NodeKey, receiver KVPairReceiver) error {
+func (ndb *nodeDB) extractStateChanges(prevVersion int64, prevRoot, root []byte, receiver KVPairReceiver) error {
 	curIter, err := NewNodeIterator(root, ndb)
 	if err != nil {
 		return err
@@ -66,7 +66,7 @@ func (ndb *nodeDB) extractStateChanges(prevVersion int64, prevRoot *NodeKey, roo
 		sharedNode = nil
 		for curIter.Valid() {
 			node := curIter.GetNode()
-			shared := node.nodeKey.version <= prevVersion
+			shared := node.version <= prevVersion
 			curIter.Next(shared)
 			if shared {
 				sharedNode = node
--- diff_test.go
+++ diff_test.go
@@ -8,7 +8,7 @@ import (
 	"sort"
 	"testing"
 
-	db "github.com/cosmos/cosmos-db"
+	db "github.com/cometbft/cometbft-db"
 	"github.com/stretchr/testify/require"
 )
 
@@ -30,7 +30,7 @@ func TestDiffRoundTrip(t *testing.T) {
 	// extract change sets from db
 	var extractChangeSets []*ChangeSet
 	tree2 := NewImmutableTree(db, 0, true)
-	err = tree2.TraverseStateChanges(0, math.MaxInt64, func(version int64, changeSet *ChangeSet) error {
+	err = tree2.ndb.traverseStateChanges(0, math.MaxInt64, func(version int64, changeSet *ChangeSet) error {
 		extractChangeSets = append(extractChangeSets, changeSet)
 		return nil
 	})
