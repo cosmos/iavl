76811eb6e9e0d43a22f2af0455c2bbdcef82d20d
Cool Developer <cool199966@outlook.com>
2023-11-30 09:30:08 -0500

    implement the diff traverse

---
+++ pruning_test.go
@@ -0,0 +1,58 @@
+package iavl
+
+import (
+	"testing"
+
+	"github.com/cosmos/iavl/v2/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTraverseOrphans(t *testing.T) {
+	pool := NewNodePool()
+	sql, err := NewSqliteDb(pool, defaultSqliteDbOptions(SqliteDbOptions{Path: t.TempDir()}))
+	require.NoError(t, err)
+	tree := NewTree(sql, pool, TreeOptions{CheckpointInterval: 1})
+
+	opts := testutil.NewTreeBuildOptions()
+	itr := opts.Iterator
+	orphans := make(map[NodeKey]bool)
+	fn := func(node *Node) error {
+		orphans[node.nodeKey] = true
+		return nil
+	}
+
+	for ; itr.Valid(); err = itr.Next() {
+		require.NoError(t, err)
+		version := itr.Version()
+		if version > 100 {
+			break
+		}
+
+		nodes := itr.Nodes()
+		for ; nodes.Valid(); err = nodes.Next() {
+			require.NoError(t, err)
+			node := nodes.GetNode()
+			if node.Delete {
+				_, _, err = tree.Remove(node.Key)
+				require.NoError(t, err)
+			} else {
+				_, err = tree.Set(node.Key, node.Value)
+				require.NoError(t, err)
+			}
+		}
+		tempOrphans := tree.orphans
+		_, v, err := tree.SaveVersion()
+		require.NoError(t, err)
+		require.Equal(t, version, v)
+
+		if v > 1 {
+			orphans = make(map[NodeKey]bool)
+			err = tree.traverseOrphans(v-1, v, fn)
+			require.NoError(t, err)
+			require.Equal(t, len(tempOrphans), len(orphans))
+			for _, nodeKey := range tempOrphans {
+				require.True(t, orphans[nodeKey])
+			}
+		}
+	}
+}
